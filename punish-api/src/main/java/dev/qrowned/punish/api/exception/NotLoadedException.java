42eda4d846458ef62c4aba9f456fa3582e4fc2db
qrowned <admin@qrowned.dev>
2022-10-31 19:00:40 +0100

    Replace concatenation with text block in NotLoadedException

--- punish-api/src/main/java/dev/qrowned/punish/api/exception/NotLoadedException.java
+++ punish-api/src/main/java/dev/qrowned/punish/api/exception/NotLoadedException.java
@@ -2,12 +2,14 @@ package dev.qrowned.punish.api.exception;
 
 public class NotLoadedException extends IllegalStateException {
 
-    private static final String MESSAGE = "The Punish API isn't loaded yet!\n" +
-            "This could be because:\n" +
-            "  a) the SupremePunish plugin is not installed or it failed to enable\n" +
-            "  b) the plugin in the stacktrace does not declare a dependency on SupremePunish\n" +
-            "  c) the plugin in the stacktrace is retrieving the API before the plugin 'enable' phase\n" +
-            "     (call the #get method in onEnable, not the constructor!)\n";
+    private static final String MESSAGE = """
+            The Punish API isn't loaded yet!
+            This could be because:
+              a) the SupremePunish plugin is not installed or it failed to enable
+              b) the plugin in the stacktrace does not declare a dependency on SupremePunish
+              c) the plugin in the stacktrace is retrieving the API before the plugin 'enable' phase
+                 (call the #get method in onEnable, not the constructor!)
+            """;
 
     public NotLoadedException() {
         super(MESSAGE);
